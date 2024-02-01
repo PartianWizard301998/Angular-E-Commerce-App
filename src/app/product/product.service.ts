9a334f6e0cd76643507772e1452322ba7a0addb2
Vaibhav Bhute <vaibhavbhute2021@gmail.com>
2024-02-01 18:44:17 +0530

    first commit

---
+++ src/app/product/product.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private apiUrl = environment.apiUrl + "/products" ;
+
+  constructor(private http: HttpClient){ }
+  
+  getProducts() : Observable<Product[]> {
+
+    return this.http.get<Product[]>(this.apiUrl);
+    
+  }
+
+}
